ec8f792c8afe3459bb349d65ee60b4f9c54c0393
eeezi <alloy0035@hanmail.net>
2025-02-14 15:05:41 +0900

    feat: 인터뷰 엔티티 정의 (#13)

---
+++ inpeak/src/main/java/com/blooming/inpeak/interview/domain/Interview.java
@@ -0,0 +1,41 @@
+package com.blooming.inpeak.interview.domain;
+
+import com.blooming.inpeak.common.base.AuditingFields;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="interviews")
+public class Interview extends AuditingFields {
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Builder
+    private Interview(Long id, Long memberId, LocalDate startDate) {
+        this.id = id;
+        this.memberId = memberId;
+        this.startDate = startDate;
+    }
+
+    public static Interview of(Long memberId, LocalDate startDate) {
+        return Interview.builder()
+                .memberId(memberId)
+                .startDate(startDate)
+                .build();
+    }
+}
