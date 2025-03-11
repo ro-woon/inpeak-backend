bd3defb1efd3000b41b3bfc0c7b825cee023ac10
minwooKang <rkdalsdn94@gmail.com>
2025-03-11 20:40:40 +0900

    feat: 회원 관심사 등록 및 수정 API (#71)

--- inpeak/src/main/java/com/blooming/inpeak/member/domain/Member.java
+++ inpeak/src/main/java/com/blooming/inpeak/member/domain/Member.java
@@ -119,4 +119,8 @@ public class Member extends BaseEntity implements UserDetails {
     public void completeRegistration() {
         this.registrationStatus = RegistrationStatus.COMPLETED;
     }
+
+    public void completeRegistration() {
+        this.registrationStatus = RegistrationStatus.COMPLETED;
+    }
 }
