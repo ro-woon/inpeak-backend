1922656cd118300fbde7b8f60b986affd9b99f27
iminhyeong <sog0740@gmail.com>
2025-07-03 15:56:14 +0900

    refactor: 카프카 zookeeper 없는 버전으로 변경 #3

--- inpeak/src/main/java/com/blooming/inpeak/common/config/kafka/KafkaConfig.java
+++ inpeak/src/main/java/com/blooming/inpeak/common/config/kafka/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.blooming.inpeak.common.config.kafka;
 import com.blooming.inpeak.answer.dto.command.AnswerTaskMessage;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -16,10 +17,13 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, AnswerTaskMessage> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "answer-task-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
