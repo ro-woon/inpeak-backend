1b453fe4839c92904c601be9b2684b0b86d8e3a1
iminhyeong <sog0740@gmail.com>
2025-07-07 20:08:58 +0900

    feat: 평균 정답률 캐싱 추가 #6

--- inpeak/src/test/java/com/blooming/inpeak/TestRedisConfiguration.java
+++ inpeak/src/test/java/com/blooming/inpeak/TestRedisConfiguration.java
@@ -3,8 +3,13 @@ package com.blooming.inpeak;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import java.io.IOException;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import redis.embedded.RedisServer;
 import redis.embedded.core.RedisServerBuilder;
 
@@ -44,5 +49,14 @@ public class TestRedisConfiguration {
             redisServer.stop();
             isRunning = false;
         }
+    }
+
+    @Bean
+    @Primary
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+            .setAddress("redis://localhost:" + redisPort);
+        return Redisson.create(config);
     }
 }
